6f9a4158d67bccbb697ea07624cc72633cfbde13
Josareta <josareta@gmail.com>
2023-10-13 17:14:20 +0200

    Añadir Ejercicio 02

--- src/ejercicio-02/index.js
+++ src/ejercicio-02/index.js
@@ -1,1 +1,19 @@
-// Escribe tu código aquí:
+// Arrow Functions y `this`
+const person = {
+    name: 'John',
+    greet : function () {
+      console.log(`Hola, soy ${this.name}`)
+    }
+  }
+  
+  person.greet() // Hola, soy John
+  
+  // Cambiando a una arrow function
+  const person2 = {
+    name: 'Alice',
+    greet: () => {
+      console.log(`Hola, soy ${this.name}`)
+    }
+  }
+  
+  person2.greet() // Hola, soy undefined (no cambia el valor de 'this')
