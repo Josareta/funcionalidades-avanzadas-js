9549c46c07d7a7701440c82b04a1130ad303d378
Josareta <josareta@gmail.com>
2023-10-14 13:50:07 +0200

    Añadir Ejercicio 10

--- src/ejercicio-10/index.js
+++ src/ejercicio-10/index.js
@@ -1,1 +1,13 @@
-// Escribe tu código aquí:
+// Uso de Map
+const personas = new Map()
+
+personas.set('Alice', 30)
+personas.set('Bob', 25)
+personas.set('Charlie', 35)
+
+console.log('Edad de Alice:', personas.get('Alice'))
+
+const personToRemove = 'Bob'
+personas.delete(personToRemove)
+
+console.log('Personas en el Map después de eliminar a Bob:', personas)
