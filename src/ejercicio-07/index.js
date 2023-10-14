0f85d3b0fe04286129e51e60f3a4d05afe7c7c36
Josareta <josareta@gmail.com>
2023-10-14 12:57:38 +0200

    Añadir Ejercicio 07

--- src/ejercicio-07/index.js
+++ src/ejercicio-07/index.js
@@ -1,1 +1,7 @@
-// Escribe tu código aquí:
+// Usando Spread Operator
+const arr1 = [1, 2, 3]
+const arr2 = [4, 5, 6]
+
+const combinedArray = [...arr1, ...arr2]
+
+console.log(combinedArray) // [1, 2, 3, 4, 5, 6]
